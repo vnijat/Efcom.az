813fd3aeea5404c0e178c908313f5d2265203f2a
Nijat Valiyev <nicatveliyev@gmail.com>
2025-03-29 18:27:53 +0400

    rmvtst

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,27 +1,11 @@
 
 import Head from 'next/head';
-import {attributes, react as HomeContent} from './../app/content/home.md';
 
 
 export default function Testimonials() {
-  let {title, cats} = attributes as {title: string, cats: {name: string, description: string;}[];};
   return (
     <>
-      <Head>
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-      </Head>
-      <article>
-        <h1>{title}</h1>
-        <HomeContent />
-        <ul>
-          {cats.map((cat, k) => (
-            <li key={k}>
-              <h2>{cat.name}</h2>
-              <p>{cat.description}</p>
-            </li>
-          ))}
-        </ul>
-      </article>
+   
     </>
   );
 }
