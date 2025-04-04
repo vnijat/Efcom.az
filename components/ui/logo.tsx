da36e058b882158ba9798780242d3b194ed594c5
Nijat Valiyev <nicatveliyev@gmail.com>
2025-04-04 22:12:16 +0400

    logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image';
 import Link from 'next/link'
 
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
-      
+       <Image
+       src={'/images/logo.png'}
+       alt="Logo"
+       width={100}
+       height={50}
+       className="h-15 w-auto"
+      />
     </Link>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        emerald:{
+          100: '#F0FFF4',
+          200: '#BBF7D0',
+          300: '#86EFAC',
+          400: '#4ADE80',
+          500: '#22C55E',
+          600: '#16A34A',
+          700: '#15803D',
+          800: '#166534',
+          900: '#14532D',
+        },
         gray: {
           100: '#FBFBFB',
           200: '#EAEAEA',
